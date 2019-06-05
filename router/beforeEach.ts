1253ed913fe4b7ba229f419df07143d8c699707b
Eugene <gl.evhen@gmail.com>
2019-06-05 13:55:55 +0200

    #5 - send post request with cart data to vs-api before redirect to magento checkout

--- router/beforeEach.ts
+++ router/beforeEach.ts
@@ -2,6 +2,7 @@ import {Route} from 'vue-router'
 import rootStore from '@vue-storefront/core/store'
 import config from 'config'
 import {storeCodeFromRoute} from '@vue-storefront/core/lib/multistore'
+import axios from 'axios'
 
 export function beforeEach(to: Route, from: Route, next) {
 
@@ -9,21 +10,39 @@ export function beforeEach(to: Route, from: Route, next) {
   const userToken: string = rootStore.state.user.token;
   const externalCheckoutConfig = {...config.externalCheckout};
   const cmsUrl: string = externalCheckoutConfig.cmsUrl;
+  const cmsCartSyncUrl: string = externalCheckoutConfig.cmsCartSyncUrl;
+  const magentoOrderEndpoint: string = externalCheckoutConfig.magentoOrderEndpoint;
+  const postRequestEnabled: boolean = externalCheckoutConfig.usePostRequest;
   const stores = externalCheckoutConfig.stores;
-  const storeCode = storeCodeFromRoute(to)
-  const multistoreEnabled: boolean = config.storeViews.multistore
+  const storeCode = storeCodeFromRoute(to);
+  const multistoreEnabled: boolean = config.storeViews.multistore;
+
+  let cartData = rootStore.state.cart;
 
   if (multistoreEnabled) {
     if (storeCode in stores && to.name === storeCode + '-checkout') {
-      window.location.replace(stores[storeCode].cmsUrl + '/vue/cart/sync/token/' + userToken + '/cart/' + cartToken)
+      syncCart(stores[storeCode].cmsUrl + '/vue/cart/sync/token/' + userToken + '/cart/' + cartToken)
     } else {
       next()
     }
   } else {
     if (to.name === 'checkout') {
-      window.location.replace(cmsUrl + '/vue/cart/sync/token/' + userToken + '/cart/' + cartToken)
+      syncCart(cmsUrl + '/vue/cart/sync/token/' + userToken + '/cart/' + cartToken)
     } else {
       next()
+    }
+  }
+
+  function syncCart(redirectUrl) {
+    if (postRequestEnabled) {
+      axios.post(magentoOrderEndpoint, {'cartData': cartData, 'cmsCartSyncUrl': cmsCartSyncUrl})
+        .then(function (response) {
+          window.location.replace(redirectUrl)
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
     }
   }
 }
